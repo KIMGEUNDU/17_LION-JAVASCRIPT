aa5b76a064255dde28b5e7b219dac8c07ba34356
KIMGEUNDU <vlkoklv@naver.com>
2023-07-07 03:18:55 +0900

    예시 학습

--- client/chapter/core/08-1.condition.js
+++ client/chapter/core/08-1.condition.js
@@ -133,3 +133,24 @@ function Button({color,name,isActive}){
 // 조건부 연산자
 
 // 멀티 조건부 연산자 식
+
+let message;
+let login = '직원';
+
+if (login == '직원') {
+  message = '안녕하세요.';
+} else if (login == '임원') {
+  message = '환영합니다.';
+} else if (login == '') {
+  message = '로그인이 필요합니다.';
+} else {
+  message = '';
+}
+
+login == '직원'
+  ? '안녕하세요'
+  : login == '임원'
+  ? '환영합니다'
+  : login == ''
+  ? '로그인이 필요합니다'
+  : '';
--- client/chapter/core/08-2.condition.js
+++ client/chapter/core/08-2.condition.js
@@ -24,3 +24,27 @@ let whichFalsy = true && ' ' && [] && { thisIsFalsy: false };
 // 첫번째 Truthy를 찾는 연산 (||)
 let whichTruthy = false || '' || [2, 3].length || { thisIsTruthy: true };
 //첫번째 true값인 [2,3].length 의 값인 2가 출력된다.
+
+// age(나이)가 14세 이상 90세 이하에 속하는지를 확인하는 if문을 작성하세요.
+let age = 13;
+
+if (age >= 14 && age <= 90) {
+  console.log('신청가능합니다');
+} else {
+  console.log('조건이 안맞습니다.');
+}
+
+//age(나이)가 14세 이상 90세 이하에 속하지 않는지를 확인하는 if문을 작성하세요.
+let age2 = 100;
+
+if (age2 < 14 || age2 > 90) {
+  console.log('신청가능합니다');
+} else {
+  console.log('조건이 안맞습니다.');
+}
+
+// 로그인 구현하기
+let Admin = prompt('비밀번호를 입력해주세요', '');
+let TheMaster = 1234;
+
+Admin == TheMaster ? console.log('환영합니다!') : console.log('취소되었습니다');
