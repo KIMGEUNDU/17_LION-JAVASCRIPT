6dfacede69c44bc579bc7d8f36c4f9b00412d669
KIMGEUNDU <vlkoklv@naver.com>
2023-07-25 03:00:34 +0900

    memo.js 수정

--- client/lib/utils/memo.js
+++ client/lib/utils/memo.js
@@ -1,24 +1,27 @@
-import { getNode } from '../dom/getNode.js';
+import { getNode } from '../dom/index.js';
 
 const cache = {};
 
 export const memo = (key, callback) => {
-  if (!callback) return cache[key];
+  if (!callback) return cache[key]; //getter, setter 구분
 
   if (cache[key]) {
     console.warn(`${key}는 이미 캐시된 값이 존재합니다.`);
     return cache[key];
-  }
+  } // 이미 같은 key 값이 있는 경우
 
   cache[key] = callback();
-
-  console.log(cache);
 };
 
-memo('cube', () => getNode('#cube'));
-memo('cube', () => 123);
-
-console.log(memo('cube'));
-console.log(cache);
-
 // 객체를 만들어서 key:value 쌍으로 저장
+
+memo('cube', () => getNode('#cube'));
+memo('cube');
+
+/*
+function(){
+return getNode('#cube')
+}
+*/
+
+memo('cube', () => 123);
