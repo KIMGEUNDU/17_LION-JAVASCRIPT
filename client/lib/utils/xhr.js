4415d9bacb142649b69bcecf48d61a54ba7a1b59
KIMGEUNDU <vlkoklv@naver.com>
2023-07-25 16:39:51 +0900

    7월 25일 수업

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -12,33 +12,100 @@
 
 /* callback --------------------------------------------- */
 
-function xhr(method, url, onSuccess, onFail) {
-  const xhr = new XMLHttpRequest();
+// 객체 구조 분해 할당
 
+export function xhr({
+  method = 'GET',
+  url = '',
+  onSuccess = null,
+  onFail = null,
+  body = null,
+  headers = {
+    'Content-Type': 'application.json',
+    'Access-Control-Allow-Origin': '*',
+  },
+} = {}) {
+  const xhr = new XMLHttpRequest();
   xhr.open(method, url);
+
+  Object.entries(headers).forEach(([key, value]) => {
+    xhr.setRequestHeader(key, value);
+  });
+
   xhr.addEventListener('readystatechange', () => {
     const { status, readyState, response } = xhr;
-    if (status >= 200 && status < 400) {
-      if (readyState === 4) {
+    if (readyState === 4) {
+      if (status >= 200 && status < 400) {
         onSuccess(JSON.parse(response));
+      } else {
+        onFail('실패');
       }
-    } else {
-      onFail('실패');
     }
   });
 
-  xhr.send();
+  xhr.send(JSON.stringify(body));
 }
 
-xhr(
-  'GET',
-  'https://jsonplaceholder.typicode.com/user',
-  (result) => {
-    console.log(result);
-  },
-  (err) => {
-    console.log(err);
-  }
-);
+// method, url, onSuccess, onFail, body, headers
 
-// 유저랜더링(data)
+// xhr({
+//   method:'PUT',
+//   url:'https://jsonplaceholder.typicode.com/users',
+//   onSuccess(result){
+//     console.log( result );
+//   },
+//   onFail(err){
+//     console.log( err );
+//   },
+//   body:{
+//     name:'tiger'
+//   },
+// });
+
+// 1. 자바스크립트의 함수는 객체다.
+// 2. 사용자(협업개발자) 입장 : 쉽게 쓰자
+// 3. 설계자 -> 함수 안에 메서드(객체)를 넣어 버리자 !!
+
+/**
+ *
+ * @param {string} url 서버와 통신할 주소
+ * @param {function} onSuccess 서버와 통신 성공시 실행될 콜백 함수
+ * @param {function} onFail 서버와의 통신 실패시 실행될 콜백 함수
+ * @return server data
+ */
+xhr.get = (url, onSuccess, onFail) => {
+  xhr({
+    url,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.post = (url, body, onSuccess, onFail) => {
+  xhr({
+    method: 'POST',
+    url,
+    body,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.put = (url, body, onSuccess, onFail) => {
+  xhr({
+    method: 'PUT',
+    url,
+    body,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.delete = (url, onSuccess, onFail) => {
+  xhr({
+    method: 'DELETE',
+    url,
+    onSuccess,
+    onFail,
+  });
+};
