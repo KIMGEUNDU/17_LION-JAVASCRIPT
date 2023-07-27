d0fa01663a6bdf0b70acbfd92d21e8fff479d89e
KIMGEUNDU <vlkoklv@naver.com>
2023-07-27 14:41:23 +0900

    07.storage 브랜치 생성

--- client/lib/utils/index.js
+++ client/lib/utils/index.js
@@ -6,7 +6,12 @@
 
 // re-export
 
-export * from './typeOf.js';
+export * from './xhr.js';
 export * from './copy.js';
+export * from './memo.js';
+export * from './color.js';
+export * from './delay.js';
+export * from './tiger.js';
+export * from './typeOf.js';
 
 
